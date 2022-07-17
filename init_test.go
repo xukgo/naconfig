261c1dc254b6074f24eb63d28e3d163e15d50bd6
xukuan <472949732@qq.com>
2022-07-17 22:41:13 +0800

    adapt for nacos new sdk

--- init_test.go
+++ init_test.go
@@ -1,1 +1,25 @@
 package naconfig
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestInitConfig(t *testing.T) {
+	vhs := make([]MatchVarHandler, 0, 1)
+	vhs = append(vhs, InitMatchVarHandler("configHandler", updateAppConfig))
+
+	storage := new(Repo)
+	xmlPath, _ := filepath.Abs("nacos.xml")
+	err := storage.InitFromXmlPath(xmlPath, vhs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func updateAppConfig(group, dataId, data string) {
+	fmt.Printf("get update data from: %s.%s.%s\n", group, dataId, data)
+}
